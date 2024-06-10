bf078f8b3d6da6e1b4610270cd34dfbd0a07e305
sumiaji <sumiaji08@gmail.com>
2024-06-10 21:14:24 +0700

    detik.com berita populer lanjut

--- popnewdetik/__init__.py
+++ popnewdetik/__init__.py
@@ -2,8 +2,11 @@
 Mengekstraksi data populer dari detik.com
 
 """
-def ekstrak_data():
+import requests
+from bs4 import BeautifulSoup
 
+
+def ekstrak_data():
     """
       Berita Terpopuler
       #1   Prediksi Ranking FIFA Indonesia Usai Kalahkan Vietnam Sepakbola | 1 jam yang lalu
@@ -13,24 +16,40 @@ def ekstrak_data():
       #5   Ganjar Gugat Suara Prabowo Nol, Muzani: Yang ke TPS Nggak Dihargai? detikNews | 2 jam yang lalu
       :return:
     """
+    url = 'https://detik.com'
+    content = requests.get(url)
+    if content.status_code == 200:
+        soup = BeautifulSoup(content.text, 'html.parser')
+        result = soup.find('div', {'class': 'box cb-mostpop'})
+        judul= result.find('h2')
+        print(judul.text)
 
-    view= dict()
-    view['satu']= 'Prediksi Ranking FIFA Indonesia Usai Kalahkan Vietnam, Sepakbola | 1 jam yang lalu'
-    view['dua']= 'Shin Tae-yong Sebut Indonesia Sedikit Beruntung,  Sepakbola | 1 jam yang lalu'
-    view['tiga']= 'Jadwal Imsak Hari Ini 27 Maret di Bogor, Depok, Bekasi, Tangerang, detikNews | 1 jam yang lalu'
-    view['empat']= 'Jadwal Imsak Hari Ini di Jakarta dan Sekitarnya, Rabu 27 Maret 2024,  detikNews | 1 jam yang lalu'
-    view['lima']= 'Ganjar Gugat Suara Prabowo Nol, Muzani: Yang ke TPS Nggak Dihargai? ,   detikNews | 2 jam yang lalu'
+        result = soup.find('div', {'class': 'box cb-mostpop'})
+        tag= result.find_all('div',{'class':'list-content'})
 
-    return view
+        print(tag)
+
+        view = dict()
+        view['satu'] = 'Prediksi Ranking FIFA Indonesia Usai Kalahkan Vietnam, Sepakbola | 1 jam yang lalu'
+        view['dua'] = 'Shin Tae-yong Sebut Indonesia Sedikit Beruntung,  Sepakbola | 1 jam yang lalu'
+        view['tiga'] = 'Jadwal Imsak Hari Ini 27 Maret di Bogor, Depok, Bekasi, Tangerang, detikNews | 1 jam yang lalu'
+        view[
+            'empat'] = 'Jadwal Imsak Hari Ini di Jakarta dan Sekitarnya, Rabu 27 Maret 2024,  detikNews | 1 jam yang lalu'
+        view[
+            'lima'] = 'Ganjar Gugat Suara Prabowo Nol, Muzani: Yang ke TPS Nggak Dihargai? ,   detikNews | 2 jam yang lalu'
+
+        return view
+    else:
+        return None
+
 
 def tampilkan_data(result):
-     print('\nBerita terpopuler dari detik.com, Indonesia')
-     print(f"#1 {result['satu']}")
-     print(f"#2 {result['dua']}")
-     print(f"#3 {result['tiga']}")
-     print(f"#4 {result['empat']}")
-     print(f"#5 {result['lima']}")
-
+    print('\nBerita terpopuler dari detik.com, Indonesia')
+    print(f"#1 {result['satu']}")
+    print(f"#2 {result['dua']}")
+    print(f"#3 {result['tiga']}")
+    print(f"#4 {result['empat']}")
+    print(f"#5 {result['lima']}")
 
 # if __name__='__main__':
 #     print('hai')
